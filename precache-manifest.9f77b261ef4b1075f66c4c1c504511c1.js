16f625efb308547de1060bb6ee0393b813aefe1c
emcreations <erdinmaulana99@gmail.com>
2019-12-22 13:11:32 +0700

    initial commit

---
+++ precache-manifest.9f77b261ef4b1075f66c4c1c504511c1.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b97f41aa4f4169ac965b6ead51dd6a17",
+    "url": "/index.html"
+  },
+  {
+    "revision": "84996c1315482a15d100",
+    "url": "/static/css/main.e3a6755f.chunk.css"
+  },
+  {
+    "revision": "26054594340dc7d298cd",
+    "url": "/static/js/2.3390174e.chunk.js"
+  },
+  {
+    "revision": "f5ca32966347038a998bbf09dda684c7",
+    "url": "/static/js/2.3390174e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "84996c1315482a15d100",
+    "url": "/static/js/main.033da62f.chunk.js"
+  },
+  {
+    "revision": "09876a21caeeabb35cf3",
+    "url": "/static/js/runtime-main.dd265961.js"
+  },
+  {
+    "revision": "d2abb862b72b21a2762c632ae9898366",
+    "url": "/static/media/desktop.d2abb862.png"
+  },
+  {
+    "revision": "5552bd0e4afb4084d01d66a3cbc8e4e5",
+    "url": "/static/media/logo.5552bd0e.svg"
+  },
+  {
+    "revision": "95e570b31fcd90f95fdfe4d65f033f9d",
+    "url": "/static/media/testimonial2.95e570b3.png"
+  },
+  {
+    "revision": "9c1a44012f05d059399eb08bf316b7f1",
+    "url": "/static/media/testimonial3.9c1a4401.png"
+  },
+  {
+    "revision": "e2cd277d0ab20457a903cfdc31d73d81",
+    "url": "/static/media/thingking-man.e2cd277d.png"
+  }
+]);
